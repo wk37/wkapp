d59540b164976e85ef2abca521ec693d206a7768
wangke <wangkeing@outlook.com>
2017-03-15 23:55:36 +0800

    导入utils工具类

--- wkapp/wksign/src/main/java/com/wangke/wksign/SignMainActivity.java
+++ wkapp/wksign/src/main/java/com/wangke/wksign/SignMainActivity.java
@@ -1,7 +1,9 @@
 package com.wangke.wksign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blankj.utilcode.utils.Utils;
 
 public class SignMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class SignMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_main);
+        Utils.init(this);
     }
 }
