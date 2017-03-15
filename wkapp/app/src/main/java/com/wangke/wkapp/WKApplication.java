3c3139b40e167d90ae3536b34a8770c5c9100431
wangke <wangkeing@outlook.com>
2017-03-15 23:32:03 +0800

    加入3个空模块，待具体实现内容

---
+++ wkapp/app/src/main/java/com/wangke/wkapp/WKApplication.java
@@ -0,0 +1,8 @@
+package com.wangke.wkapp;
+
+/**
+ * Created by wking on 2017/3/15.
+ */
+
+public class WKApplication  {
+}
---
+++ wkapp/wksign/src/main/java/com/wangke/wksign/SignMainActivity.java
@@ -0,0 +1,13 @@
+package com.wangke.wksign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_main);
+    }
+}
