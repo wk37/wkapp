40b84ac242576edc485c1cafa8b37f2dad3154f4
wk37 <keyswkii@outlook.com>
2017-03-28 19:07:56 +0800

    修改主页

--- wkapp/app/src/main/java/com/wangke/wkapp/WKMainActivity.java
+++ wkapp/app/src/main/java/com/wangke/wkapp/WKMainActivity.java
@@ -1,19 +1,51 @@
 package com.wangke.wkapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.graphics.Color;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
 
 import com.github.mzule.activityrouter.annotation.Router;
+import com.wangke.wkcore.base.BaseWkActivity;
 
 @Router("WKMainActivity")
-public class WKMainActivity extends AppCompatActivity {
+public class WKMainActivity extends BaseWkActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private AppBarLayout appBarLayout;
+    private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void setRootView() {
         setContentView(R.layout.activity_wkmain);
 
-     //
+    }
 
+    @Override
+    public void initView() {
+        toolbar = (Toolbar) findViewById(R.id.appbar_layout_toolbar);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_layout);
+
+    }
+
+    @Override
+    public void initData() {
+//        toolbar.setTitleTextColor(Color.TRANSPARENT);
+////        collapsingToolbarLayout.setTitle("");
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.white));
+        collapsingToolbarLayout.setExpandedTitleColor(Color.TRANSPARENT);
+//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+//            @Override
+//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+//                Log.e("zhouwei","appbarHeight:"+appBarLayout.getHeight()+" getTotalScrollRange:"+appBarLayout.getTotalScrollRange()+" offSet:"+verticalOffset);
+//                if(Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()){
+//                    toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+////                    collapsingToolbarLayout.setTitle("wkapp");
+//                }else{
+////                    collapsingToolbarLayout.setTitle("");
+//                }
+//            }
+//        });
     }
 }
